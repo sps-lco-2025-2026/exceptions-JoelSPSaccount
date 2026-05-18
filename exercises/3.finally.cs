db5cca0aff42a68b7b11b6172505c0886fc204ba
JoelSPSaccount <patterj@stpaulsschool.org.uk>
2026-05-18 11:52:04 +0100

    Update 3.finally.cs

--- exercises/3.finally.cs
+++ exercises/3.finally.cs
@@ -1,2 +1,22 @@
 // exercise 3
 
+try
+{
+    Console.Write("Enter a number: ");
+
+    int n = int.Parse(Console.ReadLine()!);
+
+    if (n % 2 == 0)
+        Console.WriteLine("Even");
+    else
+        Console.WriteLine("Odd");
+    
+}
+catch (FormatException)
+{
+    Console.WriteLine("Input was not in the correct format");
+}
+finally
+{
+    Console.WriteLine("Thank you for using the program.");
+}
